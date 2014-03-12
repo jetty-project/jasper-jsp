ba4c7b9e1557e8cdf3989e241b4b4ee6292b40ba
Greg Wilkins <gregw@intalio.com>
2014-03-13 08:31:22 +1100

    Squashed 'apache-jsp/src/main/' changes from 3e8bb9a..d9a7077
    d9a7077 Add non SSL version of the frame test.
    10e526c Use a regular processSocket to cleanup after an error, so that the connection is removed, etc.
    7bbef5a Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=56246 Fix NullPointerException in MemoryRealm when authenticating an unknown user.
    c9473af Switch to a semaphore here (for the flush, since the previous one would have issues if run after non blocking writes).
    6ec8484 Fix an Eclipse nag now the received message is used.
    729d7a2 Clean-up. No functional change.
    25cde82 Add some progress info to the websockets frame test.
    581cf61 Fix whitespace.
    c0426ac Add methods of get the idle time from last client access time to org.apache.catalina.Session.
    da8427a Add support for LAST_ACCESS_AT_START system property to Manager web application.
    4231dd4 Revert timeout change (no effect).
    d3dec7b - Harmonize with the NIO1 connector for upgrade close. - Actually understand where the (cosmetic) async close traces were coming from.
    e9fbf3f - Fix SSL output with blocking writes when a handshake occurs during the flow. - Fix relevant test as pointed out by Konstantin.
    4a7a9be Increase the write timeout (5s -> 10s) to see how the test reacts (or if it's a deadlock somewhere), I see some significant spikes in my own testing if I put some load on my computer.
    e05cdf7 Add a work-around for XML documents (often TLDs) that only refer to J2EE schemas by file name
    fa9b538 Correct a typo
    2d58105 Document the "execute.test.nio2" option.
    f86c6e3 Rephrase changelog message
    8ec8d93 Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=56244 Fix MBeans descriptor for WebappClassLoader MBean: The jarPath property on WebappClassLoader has been removed in Tomcat 8.
    e891fa7 Use the behavior of the NIO connector if there is a pending write when attempting a SSL close (= throw an IOE and expect a real close).
    d028b5d Fixed java.lang.NegativeArraySizeException when execute the command of Expire sessions of manager web application to context in which the session timeout is disabled.
    8cda930 Entropy attribute for Manager gone since TC 7.
    70ad7c4 - Closing is not an async operation, but needs to be performed, so ignore errors if some pending things are going on with SSL. - Also fix some warnings.
    27b2ef3 CheckStyle: fix order of imports
    9985841 Try to see the testsuite state ...
    643febf Stop using writeTimeout for now (I don't see the point at the moment).
    6b44dd9 Add code to do a clean close of all current connections.
    da37000 Fix a couple of Javadoc nags
    f6d674b Fix reported boxing warnings.
    bb680ac Fix AJP NIO2 protocol selection.
    8dbe6df Fix NIO2 mbean names tested by the registration test, it would otherwise use the NIO1 names.
    01bd301 Checkstyle: tab character
    ca4b92f Checkstyle: 1. Trailing whitespace 2. Unused import
    07f32f0 Followup to r1575905: svn:eol-style=native for new files
    84ebae3 Followup to r1575885 Do not hold writeLock when calling onError().
    27ac126 Add experimental NIO2 connector. Based on code developed by Nabil Benothman.
    a5384a0 Add more comments to explain the locks. Reduce scope of the writeLock in onWritePossible
    c6b3cb3 Add harmonized util methods for the five listener events.
    347a503 Add isBlocking util method, like in AbstractOutputBuffer.
    0a051cb Javadoc typo.
    37c8c9f Improve the robustness of web application undeployment based on some code analysis triggered by the report for BZ 54315
    
    git-subtree-dir: apache-jsp/src/main
    git-subtree-split: d9a70777abe38b9a2cf2572b5a6f1a3b980bfbb9

---
+++ java/org/apache/coyote/http11/upgrade/Nio2ServletOutputStream.java
@@ -0,0 +1,170 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.coyote.http11.upgrade;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousCloseException;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.tomcat.util.net.AbstractEndpoint;
+import org.apache.tomcat.util.net.Nio2Channel;
+import org.apache.tomcat.util.net.Nio2Endpoint;
+import org.apache.tomcat.util.net.SocketStatus;
+import org.apache.tomcat.util.net.SocketWrapper;
+
+public class Nio2ServletOutputStream extends AbstractServletOutputStream<Nio2Channel> {
+
+    private final AbstractEndpoint<Nio2Channel> endpoint;
+    private final Nio2Channel channel;
+    private final int maxWrite;
+    private final CompletionHandler<Integer, SocketWrapper<Nio2Channel>> completionHandler;
+    private final Semaphore writePending = new Semaphore(1);
+
+    public Nio2ServletOutputStream(AbstractEndpoint<Nio2Channel> endpoint0, SocketWrapper<Nio2Channel> socketWrapper) {
+        super(socketWrapper);
+        this.endpoint = endpoint0;
+        channel = socketWrapper.getSocket();
+        maxWrite = channel.getBufHandler().getWriteBuffer().capacity();
+        this.completionHandler = new CompletionHandler<Integer, SocketWrapper<Nio2Channel>>() {
+            @Override
+            public void completed(Integer nBytes, SocketWrapper<Nio2Channel> attachment) {
+                synchronized (completionHandler) {
+                    if (nBytes.intValue() < 0) {
+                        failed(new ClosedChannelException(), attachment);
+                        return;
+                    }
+                    writePending.release();
+                }
+                if (!Nio2Endpoint.isInline()) {
+                    try {
+                        onWritePossible();
+                    } catch (IOException e) {
+                        attachment.setError(true);
+                        onError(e);
+                        endpoint.processSocket(attachment, SocketStatus.ERROR, true);
+                    }
+                }
+            }
+            @Override
+            public void failed(Throwable exc, SocketWrapper<Nio2Channel> attachment) {
+                attachment.setError(true);
+                writePending.release();
+                if (exc instanceof AsynchronousCloseException) {
+                    // If already closed, don't call onError and close again
+                    return;
+                }
+                onError(exc);
+                endpoint.processSocket(attachment, SocketStatus.ERROR, true);
+            }
+        };
+    }
+
+    @Override
+    protected int doWrite(boolean block, byte[] b, int off, int len)
+            throws IOException {
+        int leftToWrite = len;
+        int count = 0;
+        int offset = off;
+
+        while (leftToWrite > 0) {
+            int writeThisLoop;
+            int writtenThisLoop;
+
+            if (leftToWrite > maxWrite) {
+                writeThisLoop = maxWrite;
+            } else {
+                writeThisLoop = leftToWrite;
+            }
+
+            writtenThisLoop = doWriteInternal(block, b, offset, writeThisLoop);
+            if (writtenThisLoop < 0) {
+                throw new EOFException();
+            }
+            count += writtenThisLoop;
+            if (!block && writePending.availablePermits() == 0) {
+                // Prevent concurrent writes in non blocking mode,
+                // leftover data has to be buffered
+                return count;
+            }
+            offset += writtenThisLoop;
+            leftToWrite -= writtenThisLoop;
+
+            if (writtenThisLoop < writeThisLoop) {
+                break;
+            }
+        }
+
+        return count;
+    }
+
+    private int doWriteInternal(boolean block, byte[] b, int off, int len)
+            throws IOException {
+        ByteBuffer buffer = channel.getBufHandler().getWriteBuffer();
+        int written = 0;
+        if (block) {
+            buffer.clear();
+            buffer.put(b, off, len);
+            buffer.flip();
+            try {
+                written = channel.write(buffer).get(socketWrapper.getTimeout(), TimeUnit.MILLISECONDS).intValue();
+            } catch (InterruptedException | ExecutionException
+                    | TimeoutException e) {
+                onError(e);
+                throw new IOException(e);
+            }
+        } else {
+            if (writePending.tryAcquire()) {
+                synchronized (completionHandler) {
+                    buffer.clear();
+                    buffer.put(b, off, len);
+                    buffer.flip();
+                    Nio2Endpoint.startInline();
+                    channel.write(buffer, socketWrapper.getTimeout(), TimeUnit.MILLISECONDS, socketWrapper, completionHandler);
+                    Nio2Endpoint.endInline();
+                    written = len;
+                }
+            }
+        }
+        return written;
+    }
+
+    @Override
+    protected void doFlush() throws IOException {
+        try {
+            // Block until a possible non blocking write is done
+            if (writePending.tryAcquire(socketWrapper.getTimeout(), TimeUnit.MILLISECONDS)) {
+                writePending.release();
+                channel.flush().get(socketWrapper.getTimeout(), TimeUnit.MILLISECONDS);
+            }
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            onError(e);
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    protected void doClose() throws IOException {
+        channel.close(true);
+    }
+}
